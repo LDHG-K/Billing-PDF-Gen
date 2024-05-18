9b51097929a26f3ce635c002913c1c81749af7bd
Luis Herrera <cookie30.ld@gmail.com>
2024-05-17 23:42:51 -0500

    Proyect creation

---
+++ src/main/java/com/billing/pdfgenerator/BillingPdfGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.billing.pdfgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingPdfGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingPdfGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billing/pdfgenerator/BillingPdfGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.pdfgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPdfGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
